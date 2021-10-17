18309961e4430e32350d216a40703e04ed8ffa9c
rpawar2823 <rpawar2823@gmail.com>
2021-10-17 14:12:45 +0530

    use specified delimiter and added test case as single and multiple negative number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -40,5 +40,29 @@ class StringCalculator {
 					.map(n -> n % 1000);
 		}
 	}
+
+	public static int sum(String input) {
+		return parseInput(input).sum();
+	}
+
+	private static StringCalculator parseInput(String input) {
+		if (input.startsWith("//")) {
+			String[] headerAndNumberSequence = input.split("\n", 2);
+			String delimiter = parseDelimiter(headerAndNumberSequence[0]);
+			return new StringCalculator(delimiter, headerAndNumberSequence[1]);
+		} else {
+			return new StringCalculator(",|\n", input);
+		}
+	}
+
+	private static String parseDelimiter(String header) {
+		String delimiter = header.substring(2);
+		if (delimiter.startsWith("[")) {
+			delimiter = delimiter.substring(1, delimiter.length() - 1);
+		}
+		return Stream.of(delimiter.split("]\\["))
+				.map(Pattern::quote)
+				.collect(Collectors.joining("|"));
+	}
     }
 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -44,4 +44,29 @@ class StringCalculatorShould {
 	public void sumsNumbersDelimitedByCommaOrNewline() {
 		assertThat(StringCalculator.sum("1,2\n3"), is(6));
 		}
+	
+	@Test
+	public void usesDelimiterSepcified() {
+		assertThat(StringCalculator.sum("//;\n1;2"), is(3));
+		assertThat(StringCalculator.sum("//.\n2.3.1"), is(6));
+		}
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void throwsOnNegativeNumber() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("negative number: -3");
+
+		StringCalculator.sum("-3");
+		}
+
+	@Test
+	public void throwsOnNegativeNumbersWithAllNumbersInExceptionMessage() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("negative number: -3,-5,-13");
+
+		StringCalculator.sum("1,-3,5,-5,-13");
+		}
 }
